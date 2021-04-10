54e5bbb5debbbaf16c5d837689bfc62ffb1322b0
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-04-10 11:42:31 -0300

    Commitando Loja

--- Java/AULAS/src/Exemplo/Turma5ProjetoLoja/src/classes/DVDS.java
+++ Java/AULAS/src/Exemplo/Turma5ProjetoLoja/src/classes/DVDS.java
@@ -1,5 +1,52 @@
 package classes;
 
-public class Dvds {
+public class Dvds extends Produto {
 
+	private int filmeDeAçao;
+	private int filmeDeAnimaçao;
+	private int filmeDeComedia;
+	private int filmeDeDrama;
+	
+	public Dvds(String descricao, String codigo, double valorUnitario, int qtdeEstoque, int filmeDeAçao,
+			int filmeDeAnimaçao, int filmeDeComedia, int filmeDeDrama) {
+		super(descricao, codigo, valorUnitario, qtdeEstoque);
+		this.filmeDeAçao = filmeDeAçao;
+		this.filmeDeAnimaçao = filmeDeAnimaçao;
+		this.filmeDeComedia = filmeDeComedia;
+		this.filmeDeDrama = filmeDeDrama;
+	}
+
+	public int getFilmeDeAçao() {
+		return filmeDeAçao;
+	}
+
+	public void setFilmeDeAçao(int filmeDeAçao) {
+		this.filmeDeAçao = filmeDeAçao;
+	}
+
+	public int getFilmeDeAnimaçao() {
+		return filmeDeAnimaçao;
+	}
+
+	public void setFilmeDeAnimaçao(int filmeDeAnimaçao) {
+		this.filmeDeAnimaçao = filmeDeAnimaçao;
+	}
+
+	public int getFilmeDeComedia() {
+		return filmeDeComedia;
+	}
+
+	public void setFilmeDeComedia(int filmeDeComedia) {
+		this.filmeDeComedia = filmeDeComedia;
+	}
+
+	public int getFilmeDeDrama() {
+		return filmeDeDrama;
+	}
+
+	public void setFilmeDeDrama(int filmeDeDrama) {
+		this.filmeDeDrama = filmeDeDrama;
+	}
+	
+	
 }
